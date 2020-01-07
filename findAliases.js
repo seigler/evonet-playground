e190035323dbc586376d826d6fa3a470c1fbface
Joshua Seigler <seigler@users.noreply.github.com>
2020-01-07 11:18:11 -0500

    feat: find names with the same ID as a given name

---
+++ findAliases.js
@@ -0,0 +1,45 @@
+const dash = require('dash');
+
+const sdkOpts = {
+  network: 'testnet'
+};
+
+if (process.argv.length != 3) {
+  throw new Error('Expected 1 argument: name');
+}
+const name = process.argv[2];
+
+const getDocuments = async function () {
+  const sdk = new dash.SDK(sdkOpts);
+  await sdk.isReady();
+  const platform = sdk.platform;
+  const originalProfile = await platform.names.get(name);
+  if (originalProfile == null) {
+    console.log(`User "${name}" not found.`);
+    return;
+  }
+  const id = originalProfile.userId;
+
+  let documents;
+  let profiles = [];
+  let startAt = 0;
+  do {
+    documents = await platform.documents.get('dpns.domain', {
+      where: [
+        ['normalizedParentDomainName', '==', 'dash'],
+        ['records.dashIdentity', '==', id]
+      ],
+      startAt: startAt,
+    });
+    profiles = profiles.concat(
+      documents.map(d => ({
+        name: d.data.normalizedLabel,
+        // nameHash: d.data.nameHash,
+        // identity: d.userId,
+      }))
+    );
+    startAt += 100;
+  } while (documents.length == 100);
+  profiles.forEach(u => console.log(u.name));
+};
+getDocuments();
