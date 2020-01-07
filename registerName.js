15b5d5e4608a101226ec11409d5464eb0ad65baa
Joshua Seigler <seigler@users.noreply.github.com>
2020-01-07 11:17:11 -0500

    feat: register a name provided by CLI

--- registerName.js
+++ registerName.js
@@ -1,17 +1,21 @@
 const DashJS = require('dash');
 require('dotenv-safe').config();
 
-const sdkOpts = {
-  network: 'testnet',
-  mnemonic: process.env.MNEMONIC,
-};
-const sdk = new DashJS.SDK(sdkOpts);
+if (process.argv.length != 3) {
+  throw new Error('Expected 1 argument: name');
+}
+const name = process.argv[2];
 
 const registerName = async function () {
+  const sdkOpts = {
+    network: 'testnet',
+    mnemonic: process.env.MNEMONIC,
+  };
+  const sdk = new DashJS.SDK(sdkOpts);
   await sdk.isReady();
   const platform = sdk.platform;
   const identity = await platform.identities.get(process.env.ID);
-  const nameRegistration = await platform.names.register('name', identity);
+  const nameRegistration = await platform.names.register(name, identity);
   console.log({nameRegistration});
 };
 registerName();
