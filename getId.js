6b65dbd251fd6a60f414d3dd03551baeb05a42a4
Joshua Seigler <seigler@users.noreply.github.com>
2020-01-20 12:14:07 -0500

    feat: look up an ID

---
+++ getId.js
@@ -0,0 +1,19 @@
+const dash = require('dash');
+
+const sdkOpts = {
+  network: 'testnet'
+};
+
+if (process.argv.length != 3) {
+  throw new Error('Expected 1 argument: id');
+}
+const id = process.argv[2];
+
+const sdk = new dash.SDK(sdkOpts);
+
+(async function () {
+  await sdk.isReady();
+  const identity = await sdk.platform.identities.get(id);
+  console.log(identity.publicKeys[0].data);
+  sdk.disconnect();
+})();
